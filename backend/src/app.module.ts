15cd32aa0a788ae5e716ccd25cec4565872c011c
vuei91 <wkwk2805@gmail.com>
2024-11-09 12:29:33 +0900

    Feat: Naming 변경

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { NotionModule } from './notion/notion.module';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), NotionModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), NotionModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
