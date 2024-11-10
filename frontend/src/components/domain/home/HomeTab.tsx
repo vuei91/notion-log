22506c340734ed1b882eaca051f4e2d98d960dd8
vuei91 <wkwk2805@gmail.com>
2024-11-10 13:33:00 +0900

    Feat: 탭 만들기

--- frontend/src/components/domain/home/HomeTab.tsx
+++ frontend/src/components/domain/home/HomeTab.tsx
@@ -1,19 +1,52 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+
+enum Tab {
+  RECOMMAND = 0,
+  ARTICLE = 1,
+  MY_FEED = 2,
+}
 
 const HomeTab = () => {
+  const tabs = [Tab.RECOMMAND, Tab.ARTICLE, Tab.MY_FEED];
+  const [selectedTab, setSelectedTab] = useState<number>(Tab.RECOMMAND);
+  const handleTabClick = (tab: number) => {
+    setSelectedTab(tab);
+  };
   return (
     <div className="flex my-[10px]">
-      <div className="flex justify-center items-center flex-col box-border w-[72px] h-[48px] text-[#0066FF]">
+      <div
+        className={`flex justify-center items-center flex-col box-border w-[72px] h-[48px] cursor-pointer text-[#0066FF] ${
+          selectedTab === Tab.RECOMMAND ? "text-[#0066FF]" : "text-[#767676]"
+        }`}
+        onClick={() => handleTabClick(Tab.RECOMMAND)}
+      >
         <div>추천</div>
-        <div className="w-[32px] h-[4px] bg-[#0066FF]"></div>
+        {selectedTab === Tab.RECOMMAND && (
+          <div className="w-[32px] h-[4px] bg-[#0066FF]"></div>
+        )}
       </div>
-      <div className="flex justify-center items-center flex-col box-border w-[72px] h-[48px]">
+      <div
+        className={`flex justify-center items-center flex-col box-border w-[72px] h-[48px] cursor-pointer ${
+          selectedTab === Tab.ARTICLE ? "text-[#0066FF]" : "text-[#767676]"
+        }`}
+        onClick={() => handleTabClick(Tab.ARTICLE)}
+      >
         <div>아티클</div>
-        <div className="w-[45px] h-[4px] bg-[#0066FF]"></div>
+        {selectedTab === Tab.ARTICLE && (
+          <div className="w-[45px] h-[4px] bg-[#0066FF]"></div>
+        )}
       </div>
-      <div className="flex justify-center items-center flex-col box-border w-[72px] h-[48px]">
+      <div
+        className={`flex justify-center items-center flex-col box-border w-[72px] h-[48px] cursor-pointer ${
+          selectedTab === Tab.MY_FEED ? "text-[#0066FF]" : "text-[#767676]"
+        }`}
+        onClick={() => handleTabClick(Tab.MY_FEED)}
+      >
         <div>내 피드</div>
-        <div className="w-[50px] h-[4px] bg-[#0066FF]"></div>
+        {selectedTab === Tab.MY_FEED && (
+          <div className="w-[50px] h-[4px] bg-[#0066FF]"></div>
+        )}
       </div>
     </div>
   );
