da5be96bb588c9e80ded0eadab3e64b602a97207
vuei91 <wkwk2805@gmail.com>
2024-11-13 18:28:51 +0900

    Refact: notion detail data 리팩토링

--- frontend/src/app/notion/about/page.tsx
+++ frontend/src/app/notion/about/page.tsx
@@ -1,42 +1,19 @@
 import { encrypt } from "@/utils/crypto";
-import { notion } from "@/utils/notion";
+import { getNotionDetail } from "@/utils/notion";
 import { redirect } from "next/navigation";
-import {
-  defaultMapImageUrl,
-  getPageImageUrls,
-  getPageTitle,
-  getTextContent,
-  parsePageId,
-} from "notion-utils";
 
 const About = async ({ searchParams }: { searchParams: { id: string } }) => {
   let redirectUrl = "/";
-
   try {
     // notion checking
     const url = searchParams.id;
-    const pageId = parsePageId(url);
-    if (!pageId) return;
-    const page = await notion.getPage(pageId);
-    if (!page) return;
-    const title = getPageTitle(page);
-    const images = getPageImageUrls(page, {
-      mapImageUrl: defaultMapImageUrl as any,
-    });
-    const text = [];
-    for (const block in page.block) {
-      text.push(getTextContent(page.block[block].value?.properties?.title));
-    }
-    const description = text.join("\n");
-    const result = {
-      title,
-      description,
-      thumbnail: images[0],
-      url,
-    };
-    redirectUrl =
-      "/notion/create/?data=" +
-      encrypt(JSON.stringify(result), process.env.NEXT_PUBLIC_AES_SCERET_KEY!);
+    const result = await getNotionDetail(url);
+    if (!result) return;
+    const encResult = encrypt(
+      JSON.stringify(result),
+      process.env.NEXT_PUBLIC_AES_SCERET_KEY!,
+    );
+    redirectUrl = `/notion/create/?data=${encResult}`;
   } catch (error) {
     console.error(error);
   }
--- frontend/src/utils/notion.ts
+++ frontend/src/utils/notion.ts
@@ -1,3 +1,36 @@
+import { Notion } from "@/types";
 import { NotionAPI } from "notion-client";
+import {
+  defaultMapImageUrl,
+  getPageImageUrls,
+  getPageTitle,
+  getTextContent,
+  parsePageId,
+} from "notion-utils";
 
 export const notion = new NotionAPI();
+
+export const getNotionDetail = async (
+  url: string,
+): Promise<Partial<Notion> | undefined> => {
+  const pageId = parsePageId(url);
+  if (!pageId) return;
+  const page = await notion.getPage(pageId);
+  if (!page) return;
+  const title = getPageTitle(page)!;
+  const images = getPageImageUrls(page, {
+    mapImageUrl: defaultMapImageUrl as any,
+  });
+  const text = [];
+  for (const block in page.block) {
+    text.push(getTextContent(page.block[block].value?.properties?.title));
+  }
+  const description = text.join("\n");
+  const result = {
+    title,
+    description,
+    thumbnail: images[0],
+    url,
+  };
+  return result;
+};
