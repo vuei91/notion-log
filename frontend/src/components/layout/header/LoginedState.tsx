2064b4c5f1744cb96d4a7bc641e1ff111f289d4f
vuei91 <wkwk2805@gmail.com>
2024-11-14 15:09:52 +0900

    Feat: 로그아웃 버튼 누를수 있도록 늦게 사라지도록 처리

--- frontend/src/components/layout/header/LoginedState.tsx
+++ frontend/src/components/layout/header/LoginedState.tsx
@@ -54,7 +54,7 @@ const LoginedState = ({
       </button>
       <div className="group relative flex gap-[5px]">
         <AvartarL url={user.user_metadata.picture} />
-        <div className="invisible absolute right-0 top-[50px] z-[201] box-border w-[80px] rounded-lg border-[1px] border-[#D9D9D9] bg-white p-[10px] text-center hover:visible group-hover:visible">
+        <div className="invisible absolute right-0 top-[50px] z-[201] box-border w-[80px] rounded-lg border-[1px] border-[#D9D9D9] bg-white p-[10px] text-center delay-200 hover:visible group-hover:visible">
           <button onClick={logout}>로그아웃</button>
         </div>
       </div>
