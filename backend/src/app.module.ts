938b5077ea14395230e2e7e1f0ff81912fad9323
vuei91 <wkwk2805@gmail.com>
2024-11-15 16:19:35 +0900

    Feat: search2 제거

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,10 +3,9 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { SearchModule } from './search/search.module';
-import { Search2Module } from './search2/search2.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), SearchModule, Search2Module],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), SearchModule],
   controllers: [AppController],
   providers: [AppService],
 })
