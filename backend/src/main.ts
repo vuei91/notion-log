9d0d86b15671a3e1f02dd1299ee0060726ca3d3b
vuei91 <wkwk2805@gmail.com>
2024-11-28 12:22:26 +0900

    Feat: Nextjs로만 만듦

--- backend/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  console.log(process.env.PORT);
-  await app.listen(process.env.PORT ?? 3000);
-}
-bootstrap();
